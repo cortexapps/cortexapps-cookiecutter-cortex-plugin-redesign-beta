11aa6c331347a0dcfa47f3562066b58e7f634454
Dan Schuman <dan.schuman@cortex.io>
2025-04-04 16:10:53 -0700

    Add example color desc

--- {{cookiecutter.project_name}}/src/components/ColorSwatches.tsx
+++ {{cookiecutter.project_name}}/src/components/ColorSwatches.tsx
@@ -13,51 +13,57 @@ export const ColorSwatches: React.FC = () => (
     </CardHeader>
     <CardContent className="flex flex-col gap-1">
       <div className="swatch-container">
-        <div>--cortex-plugin-background:</div>
+        <div>
+          --cortex-plugin-background (Primary background (black/white)):
+        </div>
         <div className="swatch swatch--cortex-plugin-background" />
       </div>
       <div className="swatch-container">
-        <div>--cortex-plugin-foreground:</div>
+        <div>
+          --cortex-plugin-foreground (Primary foreground (white/black)):
+        </div>
         <div className="swatch swatch--cortex-plugin-foreground" />
       </div>
       <div className="swatch-container">
-        <div>--cortex-plugin-primary:</div>
+        <div>
+          --cortex-plugin-primary (Text color (contrasts with background)):
+        </div>
         <div className="swatch swatch--cortex-plugin-primary" />
       </div>
       <div className="swatch-container">
-        <div>--cortex-plugin-secondary:</div>
+        <div>
+          --cortex-plugin-secondary (Text color (contrasts with background)):
+        </div>
         <div className="swatch swatch--cortex-plugin-secondary" />
       </div>
       <div className="swatch-container">
-        <div>--cortex-plugin-muted:</div>
+        <div>
+          --cortex-plugin-muted (Text color (contrasts with background)):
+        </div>
         <div className="swatch swatch--cortex-plugin-muted" />
       </div>
       <div className="swatch-container">
-        <div>--cortex-plugin-accent:</div>
+        <div>--cortex-plugin-accent (Brand color (configurable)):</div>
         <div className="swatch swatch--cortex-plugin-accent" />
       </div>
       <div className="swatch-container">
-        <div>--cortex-plugin-accent:</div>
-        <div className="swatch swatch--cortex-plugin-accent" />
-      </div>
-      <div className="swatch-container">
-        <div>--cortex-plugin-destructive:</div>
+        <div>--cortex-plugin-destructive (Error display background):</div>
         <div className="swatch swatch--cortex-plugin-destructive" />
       </div>
       <div className="swatch-container">
-        <div>--cortex-plugin-destructive-foreground:</div>
+        <div>--cortex-plugin-destructive-foreground (Error display text):</div>
         <div className="swatch swatch--cortex-plugin-destructive-foreground" />
       </div>
       <div className="swatch-container">
-        <div>--cortex-plugin-border:</div>
+        <div>--cortex-plugin-border (Border color):</div>
         <div className="swatch swatch--cortex-plugin-border" />
       </div>
       <div className="swatch-container">
-        <div>--cortex-plugin-input:</div>
+        <div>--cortex-plugin-input (Input background):</div>
         <div className="swatch swatch--cortex-plugin-input" />
       </div>
       <div className="swatch-container">
-        <div>--cortex-plugin-ring:</div>
+        <div>--cortex-plugin-ring (Focus ring color):</div>
         <div className="swatch swatch--cortex-plugin-ring" />
       </div>
     </CardContent>
